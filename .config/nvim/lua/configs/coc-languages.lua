8dee23d94cf3dfdac2348b976ef11577700faec2
Kevin Norman <spaxtersurvival@gmail.com>
2024-11-01 08:09:02 +0100

    move coc languages to separate file

---
+++ .config/nvim/lua/configs/coc-languages.lua
@@ -0,0 +1,16 @@
+return {
+    "coc-json",
+    "coc-python",
+    "coc-lua",
+    "coc-tsserver",
+    "coc-html",
+    "coc-css",
+    "coc-pyright",
+    "coc-java",
+    "coc-snippets",
+    "coc-pairs",
+    "coc-eslint",
+    "coc-prettier",
+    "coc-svelte",
+    "@yaegassy/coc-tailwindcss3"
+}
