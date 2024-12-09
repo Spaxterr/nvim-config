013897d53c1e4615769c23c1bea93c7a82cc755d
Kevin Norman <spaxtersurvival@gmail.com>
2024-12-09 09:12:51 +0100

    replace none-ls with nvim-lint

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,6 +1,7 @@
 -- Automatically load last session and close unrelated files
 vim.api.nvim_create_autocmd("User", {
     pattern = "PersistedTelescopeLoadPre",
+
     callback = function(session)
         -- Save the currently loaded session using the global variable
         require("persisted").save({ session = vim.g.persisted_loaded_session })
@@ -12,7 +13,9 @@ vim.api.nvim_create_autocmd("User", {
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
   callback = function()
-    require("lint").try_lint()
+    local lint = require("lint")
+    lint.try_lint('trail_space')
+    lint.try_lint()
   end,
 })
 
--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -2,7 +2,33 @@ return {
     {
         'mfussenegger/nvim-lint',
         config = function ()
-            require('lint').linters_by_ft = {
+            local lint = require('lint')
+
+            lint.linters.trail_space = {
+                cmd = 'rgrep',
+                args = { '-nH', '\\s$' },
+                stdin = false,
+                ignore_exitcode = true,
+                parser = function(output, bufnr)
+                    local diagnostics = {}
+                    for line in output:gmatch("[^\r\n]+") do
+                        local file, lineno, message = line:match("^(.-):(%d+):(.*)$")
+                        if file and lineno then
+                            table.insert(diagnostics, {
+                                bufnr = bufnr,
+                                lnum = tonumber(lineno) - 1,
+                                col = 0,
+                                severity = vim.diagnostic.severity.INFO,
+                                message = "Trailing whitespace",
+                                source = "trail_space",
+                            })
+                        end
+                    end
+                    return diagnostics
+                end,
+            }
+
+            lint.linters_by_ft = {
                 javascript = {'eslint'},
                 typescript = {'eslint'},
                 svelte = {'eslint'},
