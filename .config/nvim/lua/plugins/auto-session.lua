31d043a020666f1d23cad5305222eba7aad3d24e
Kevin Norman <kevin.norman@mtek.se>
2025-01-17 09:32:37 +0000

    replace persisted with auto-session

---
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,13 @@
+return {
+    'rmagatti/auto-session',
+    lazy = false,
+    config = function()
+        require('auto-session').setup({
+            enabled = true,
+            auto_save = true,
+            auto_restore = true,
+            auto_create = true,
+            suppressed_dirs = { '~/', '/tmp' },
+        })
+    end
+}
