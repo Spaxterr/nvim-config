a5338ebc5aeb6aed067961485a8e089856b4343a
Kevin Norman <kevin.norman@mtek.se>
2025-01-29 09:15:39 +0000

    format files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,26 +4,25 @@ vim.opt.fileencoding = "utf-8"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.tabufline_enabled = false
 
 -- vim.g.coc_global_extensions = require('configs.coc-languages')
-vim.g.svelte_preprocessors = { 'typescript' }
-
+vim.g.svelte_preprocessors = { "typescript" }
 
 -- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
     local repo = "https://github.com/folke/lazy.nvim.git"
-    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+    vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
+local lazy_config = require("configs.lazy")
 
 -- load plugins
 require("lazy").setup({
@@ -37,7 +36,7 @@ require("lazy").setup({
     { import = "plugins" },
 }, lazy_config)
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
     enabled = false, -- Disable nvim-cmp since coc already has autocompletion
 })
@@ -46,24 +45,36 @@ cmp.setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-require "options"
-require "nvchad.autocmds"
-require "autocmds"
+require("options")
+require("nvchad.autocmds")
+require("autocmds")
 
 vim.schedule(function()
-    require "mappings"
+    require("mappings")
 end)
 
-vim.api.nvim_create_user_command('CloseAllBuffers', function()
+vim.api.nvim_create_user_command("CloseAllBuffers", function()
     for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
         if vim.api.nvim_buf_is_valid(bufnr) and not vim.bo[bufnr].modified then
-            vim.cmd('bdelete ' .. bufnr)
+            vim.cmd("bdelete " .. bufnr)
         end
     end
 end, {})
 
+vim.api.nvim_create_user_command("Format", function(args)
+    local range = nil
+    if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+        }
+    end
+    require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
+
 -- Set inline hints to the same color as LineNr
-vim.cmd [[highlight! link CocInlayHint LineNr ]]
+vim.cmd([[highlight! link CocInlayHint LineNr ]])
 
 -- Set highlight colors for notifier
 vim.cmd([[
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -13,58 +13,86 @@ map("n", "<C-u>", "<C-u>zz", { desc = "Move up half a page and re-center", remap
 map("n", "<C-d>", "<C-d>zz", { desc = "Move down half a page and re-center", remap = true })
 
 -- Neogen
-map("n", "<Leader>cdf", ":lua require('neogen').generate({ type = 'func' })<CR>",
-    { noremap = true, silent = true, desc = "Generate function docstring" })
-map("n", "<Leader>cdc", ":lua require('neogen').generate({ type = 'class' })<CR>",
-    { noremap = true, silent = true, desc = "Generate class docstring" })
-map("n", "<Leader>cdt", ":lua require('neogen').generate({ type = 'type' })<CR>",
-    { noremap = true, silent = true, desc = "Generate type docstring" })
+map(
+    "n",
+    "<Leader>cdf",
+    ":lua require('neogen').generate({ type = 'func' })<CR>",
+    { noremap = true, silent = true, desc = "Generate function docstring" }
+)
+map(
+    "n",
+    "<Leader>cdc",
+    ":lua require('neogen').generate({ type = 'class' })<CR>",
+    { noremap = true, silent = true, desc = "Generate class docstring" }
+)
+map(
+    "n",
+    "<Leader>cdt",
+    ":lua require('neogen').generate({ type = 'type' })<CR>",
+    { noremap = true, silent = true, desc = "Generate type docstring" }
+)
 
 -- Disable spacebar moving cursor as it is used as leader key
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { silent = true, noremap = true })
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { silent = true, noremap = true })
 
 -- Set '<Space> + c + w' to clear trailing whitespace
-map({ 'v', 'n' }, '<Leader>cw', [[:<C-u>'<,'>s/\s\+$//<CR>]],
-    { noremap = true, silent = true, desc = "Remove trailing whitespaces" });
+map(
+    { "v", "n" },
+    "<Leader>cw",
+    [[:<C-u>'<,'>s/\s\+$//<CR>]],
+    { noremap = true, silent = true, desc = "Remove trailing whitespaces" }
+)
 
 -- Set 'b+c' to close all open tabs/buffers
-vim.keymap.set('n', '<leader>bc', ':CloseAllBuffers<CR>', { noremap = true, silent = true, desc = "Close all tabs" })
+vim.keymap.set("n", "<leader>bc", ":CloseAllBuffers<CR>", { noremap = true, silent = true, desc = "Close all tabs" })
 
 -- Fzf Lua
-local fzf = require('fzf-lua')
+local fzf = require("fzf-lua")
 map("n", "<leader>fw", fzf.live_grep, { noremap = true, silent = true, desc = "Live grep" })
-map('n', '<leader>ff', fzf.files, { noremap = true, silent = true, desc = "Find files" })
-map('n', '<leader>ft', fzf.treesitter, { noremap = true, silent = true, desc = "Find symbols" })
+map("n", "<leader>ff", fzf.files, { noremap = true, silent = true, desc = "Find files" })
+map("n", "<leader>ft", fzf.treesitter, { noremap = true, silent = true, desc = "Find symbols" })
 
 -- Coc bindings
-map('v', '<Leader>ff', '<Plug>(coc-format-selected)', { desc = "Format selection", silent = true, noremap = true })
-map('n', '<Leader>cf', ':call CocAction("format")<CR>', { desc = "Format file", silent = true, noremap = true })
-map('n', '<Leader>cr', '<Plug>(coc-rename)', { desc = "Rename symbol", silent = true, noremap = true })
-map('i', '<C-s>', 'coc#refresh()', { desc = "Show completions", silent = true, expr = true, noremap = true })
+map("n", "<Leader>cr", "<Plug>(coc-rename)", { desc = "Rename symbol", silent = true, noremap = true })
+map("i", "<C-s>", "coc#refresh()", { desc = "Show completions", silent = true, expr = true, noremap = true })
 map("n", "gd", "<Plug>(coc-definition)", { silent = true, desc = "Go to definition" })
 map("n", "gy", "<Plug>(coc-type-definition)", { silent = true, desc = "Go to type definition" })
 map("n", "gi", "<Plug>(coc-implementation)", { silent = true, desc = "Go to implementation" })
 map("n", "gr", "<Plug>(coc-references)", { silent = true, desc = "Go to reference(s)" })
 map({ "v", "n", "x" }, "<leader>cq", "<Plug>(coc-codeaction-cursor)", { silent = true, desc = "Show code actions" })
 map("n", "K", ":call CocActionAsync('doHover')<CR>", { silent = true, desc = "Hover" })
-map("i", "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
-    { noremap = true, silent = true, expr = true })
+map(
+    "i",
+    "<CR>",
+    [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
+    { noremap = true, silent = true, expr = true }
+)
 map("i", "<C-j>", 'coc#pum#visible() ? coc#pum#next(1) : "<C-j>"', { noremap = true, expr = true, silent = true })
 map("i", "<C-k>", 'coc#pum#visible() ? coc#pum#prev(1) : "<C-k>"', { noremap = true, expr = true, silent = true })
 
 -- Barbar bindings
-map('n', '<Tab>', '<Cmd>BufferNext<CR>', { noremap = true, silent = true, desc = "Next tab" })
-map('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true, desc = "Previous tab" })
-map('n', '<space>x', '<Cmd>BufferClose<CR>', { noremap = true, silent = true, desc = "Close file" })
+map("n", "<Tab>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true, desc = "Next tab" })
+map("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true, desc = "Previous tab" })
+map("n", "<space>x", "<Cmd>BufferClose<CR>", { noremap = true, silent = true, desc = "Close file" })
 
 -- Git
-local gitsigns = require('gitsigns')
-map('n', '<Space>gb', function() gitsigns.blame_line { full = true } end,
-    { noremap = true, silent = true, desc = "Git blame current line" })
-map('n', '<Space>gt', '<Cmd>GitBlameToggle<CR>', { noremap = true, silent = true, desc = "Toggle git blame" })
+local gitsigns = require("gitsigns")
+map("n", "<Space>gb", function()
+    gitsigns.blame_line({ full = true })
+end, { noremap = true, silent = true, desc = "Git blame current line" })
+map("n", "<Space>gt", "<Cmd>GitBlameToggle<CR>", { noremap = true, silent = true, desc = "Toggle git blame" })
 
 -- tmux navigator
-map('n', '<C-h>', '<Cmd>TmuxNavigateLeft<CR>', { noremap = true, silent = true, desc = 'Window Left' })
-map('n', '<C-l>', '<Cmd>TmuxNavigateRight<CR>', { noremap = true, silent = true, desc = 'Window Right' })
-map('n', '<C-j>', '<Cmd>TmuxNavigateDown<CR>', { noremap = true, silent = true, desc = 'Window Down' })
-map('n', '<C-k>', '<Cmd>TmuxNavigateUp<CR>', { noremap = true, silent = true, desc = 'Window Up' })
+map("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", { noremap = true, silent = true, desc = "Window Left" })
+map("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { noremap = true, silent = true, desc = "Window Right" })
+map("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { noremap = true, silent = true, desc = "Window Down" })
+map("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { noremap = true, silent = true, desc = "Window Up" })
+
+-- Conform
+map("v", "<Leader>ff", "<Cmd>Format<CR>", { desc = "Format selection", silent = true, noremap = true })
+map(
+    "n",
+    "<Leader>cf",
+    ':lua require("conform").format({ async = true, lsp_format = "fallback" })<CR>',
+    { desc = "Format file", silent = true, noremap = true }
+)
