fc272965eadff6c145e79d9dd409b3d9fd80ecaa
Kevin Norman <kevin.norman@mtek.se>
2025-01-31 09:14:19 +0000

    add flake8 and cspell

--- .config/nvim/lua/plugins/ale.lua
+++ .config/nvim/lua/plugins/ale.lua
@@ -5,25 +5,27 @@ return {
         config = function()
             local g = vim.g
 
-            g.ale_cursor_detail = 1
-            g.ale_list_window_size = 5
-            g.ale_detail_to_floating_preview = 1
-            g.ale_floating_window_border = {}
-            g.ale_set_balloons = 1
-            g.ale_floating_window_preview = 1
-            g.ale_hover_delay = 100
-            g.ale_virtualtext_cursor = 1
+            g.ale_cursor_detail = 1 -- Show a preview when hovering over an erroring line
+            g.ale_floating_preview = 1
+            g.ale_close_preview_on_insert = 1
+            g.ale_lint_on_text_changed = 'never'
+            g.ale_open_list = 0
+            g.ale_set_highlights = 1
+            g.ale_virtualtext_cursor = 'disabled'
+            g.ale_floating_window_border = {'│', '─', '╭', '╮', '╯', '╰', '│', '─'}
 
-            g.ale_warn_about_trailing_whitespace = 1
             g.ale_echo_msg_format = "[%linter%] %s (%code%)"
             g.ale_window_msg_format = "[%linter%] %s (%code%)"
-            g.ale_disable_lsp = 1
+            g.ale_disable_lsp = 1 -- Disable ALE since coc is present
             g.ale_use_neovim_diagnostics_api = 1
 
+            g.python_flake8_options = "--config ./tox.ini --verbose"
+
             g.ale_linters = {
-                lua = { "lua_language_server" },
-                javascript = { "eslint" },
-                typescript = { "eslint" },
+                lua = { "lua_language_server", "cspell" },
+                javascript = { "eslint", "cspell" },
+                typescript = { "eslint", "cspell" },
+                python = { "flake8", "cspell" },
             }
         end,
     },
