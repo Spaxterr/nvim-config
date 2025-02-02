2cd830deb63da98c76acf1691755f9b200757518
Kevin Norman <spaxtersurvival@gmail.com>
2025-02-02 17:00:04 +0100

    fix prettier config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,23 +4,57 @@ return {
         config = function()
             local conform = require("conform")
 
-            local function get_prettier_config()
-                local ctx_dirname = vim.fn.getcwd()
-                local local_prettier_config = vim.fs.find(".prettierrc", {
-                    upward = true,
-                    path = ctx_dirname,
-                    type = "file",
-                })[1]
-                local global_prettier_config = vim.fs.find(".prettierrc", {
-                    path = vim.fn.expand("~/"),
-                    type = "file",
-                })[1]
+            -- List of possible Prettier config file names.
+            local prettier_files = {
+                ".prettierrc",
+                ".prettierrc.json",
+                ".prettierrc.js",
+                ".prettierrc.cjs",
+                ".prettierrc.yaml",
+                ".prettierrc.yml",
+                "prettier.config.js",
+                "prettier.config.cjs",
+            }
 
-                if local_prettier_config then
-                    return local_prettier_config
-                elseif global_prettier_config then
-                    return global_prettier_config
+            local function find_config_in_path(path)
+                for _, filename in ipairs(prettier_files) do
+                    local results = vim.fs.find(filename, {
+                        path = path,
+                        upward = false, -- Only search in the given directory.
+                        type = "file",
+                        hidden = true, -- Include hidden files in the search.
+                    })
+                    if results then
+                        for _, file in ipairs(results) do
+                            -- Ignore any file that is in a node_modules directory.
+                            if not file:find("node_modules") then
+                                return file
+                            end
+                        end
+                    end
                 end
+                return nil
+            end
+
+            local function get_prettier_config()
+                local cwd = vim.fn.getcwd()
+
+                -- Check only the local (current) directory.
+                local local_config = find_config_in_path(cwd)
+                if local_config then
+                    print("Formatting file using", local_config)
+                    return local_config
+                end
+
+                -- Fallback: check for a global config in your home directory.
+                local home = vim.fn.expand("~")
+                local global_config = find_config_in_path(home)
+                if global_config then
+                    print("Formatting file using", global_config)
+                    return global_config
+                end
+
+                vim.notify("No prettier config found", vim.log.levels.WARN)
                 return nil
             end
 
@@ -30,6 +64,7 @@ return {
                     typescript = { "prettier" },
                     svelte = { "prettier" },
                     json = { "prettier" },
+                    html = { "prettier" },
                     lua = { "stylua" },
                     java = { "clang-format" },
                 },
