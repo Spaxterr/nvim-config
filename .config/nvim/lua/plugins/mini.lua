edd80bd842c40ea7dbb2e6c9f7de4c84fbcb03cc
Kevin Norman <kevin.norman@mtek.se>
2025-02-18 08:35:51 +0000

    rename mini plugin file

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "echasnovski/mini.nvim",
+        version = false,
+        lazy = false,
+        config = function()
+            require("mini.trailspace").setup({
+                only_in_normal_buffers = false,
+            })
+        end,
+    },
+}
