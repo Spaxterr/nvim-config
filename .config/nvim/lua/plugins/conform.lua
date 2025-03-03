b6ee68d573f6d5fad4e894f8fdcc36e6102f0b1f
Kevin Norman <spaxtersurvival@gmail.com>
2025-03-03 09:44:22 +0100

    add rustfmt formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -40,6 +40,7 @@ return {
                     python = { "black" },
                     lua = { "stylua" },
                     java = { "clang-format" },
+                    rust = { "rustfmt" },
                 },
                 formatters = {
                     prettier = {
