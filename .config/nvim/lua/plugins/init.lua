ecdc6e39a538ff071514d806d24a43439d0ffb4c
Kevin Norman <kevin.norman@mtek.se>
2025-04-01 07:11:33 +0000

    add cmp config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,15 +1,29 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "zjp-CN/nvim-cmp-lsp-rs",
+            "ray-x/cmp-sql",
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "cmp_lsp_rs" },
+                    { name = "buffer" },
+                    { name = "path" },
+                    { name = "supermaven-nvim" },
+                    { name = "sql" },
+                    { name = "dotenv" },
+                },
+            })
+        end,
+    },
 }
