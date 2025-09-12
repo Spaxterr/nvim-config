9fad58a1a15c5fc75b2fa69be79f042fcd680768
Kevin Norman <kevin.norman@mtek.se>
2025-09-12 08:30:02 +0000

    update packages

--- .config/nvim/lua/lsp/servers/ts_ls.lua
+++ .config/nvim/lua/lsp/servers/ts_ls.lua
@@ -1,69 +1,64 @@
 return {
-    settings = {
-        javascript = {
-            inlayHints = {
-                includeInlayParameterNameHints = "literals",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-            },
-            completions = {
-                completeFunctionCalls = true,
-            },
-        },
-        filetypes = {
-            "javascript",
-            "javascriptreact",
-            "typescript",
-            "typescriptreact",
-            "svelte"
-        },
-        typescript = {
-            enable = true,
-            suggest = {
-                includeCompletionsForModuleExports = true,
-                includeAutomaticOptionalChainCompletions = true,
-            },
-        },
-        diagnostics = {
-            enable = true,
-            reportStyleChecksAsWarnings = true,
-            ignoreEmit = true,
-        },
-        init_options = {
-            hostInfo = "neovim",
-            preferences = {
-                -- Import preferences
-                importModuleSpecifier = "relative",
-                importModuleSpecifierEnding = "minimal",
-                includePackageJsonAutoImports = "auto",
+	workspace_required = true,
+	root_markers = { "tsconfig.json", "package.json" },
+	settings = {
+		javascript = {
+			inlayHints = {
+				includeInlayParameterNameHints = "literals",
+				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayVariableTypeHints = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayEnumMemberValueHints = true,
+			},
+			completions = {
+				completeFunctionCalls = true,
+			},
+		},
+		typescript = {
+			enable = true,
+			suggest = {
+				includeCompletionsForModuleExports = true,
+				includeAutomaticOptionalChainCompletions = true,
+			},
+		},
+		diagnostics = {
+			enable = true,
+			reportStyleChecksAsWarnings = true,
+			ignoreEmit = true,
+		},
+		init_options = {
+			hostInfo = "neovim",
+			preferences = {
+				-- Import preferences
+				importModuleSpecifier = "relative",
+				importModuleSpecifierEnding = "minimal",
+				includePackageJsonAutoImports = "auto",
 
-                -- Code generation preferences
-                includeCompletionsForModuleExports = true,
-                includeCompletionsWithInsertText = true,
+				-- Code generation preferences
+				includeCompletionsForModuleExports = true,
+				includeCompletionsWithInsertText = true,
 
-                -- Performance optimizations
-                disableSuggestions = false,
-                allowIncompleteCompletions = true,
-                allowRenameOfImportPath = true,
+				-- Performance optimizations
+				disableSuggestions = false,
+				allowIncompleteCompletions = true,
+				allowRenameOfImportPath = true,
 
-                -- Suggest preferences
-                providePrefixAndSuffixTextForRename = true,
-                provideRefactorNotApplicableReason = true,
-            },
-        },
-    },
-    on_attach = function(client, bufnr)
-        -- Enable inlay hints if supported (Neovim 0.10+)
-        if client.server_capabilities.inlayHintProvider then
-            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-        end
+				-- Suggest preferences
+				providePrefixAndSuffixTextForRename = true,
+				provideRefactorNotApplicableReason = true,
+			},
+		},
+	},
+	on_attach = function(client, bufnr)
+		-- Enable inlay hints if supported (Neovim 0.10+)
+		if client.server_capabilities.inlayHintProvider then
+			vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+		end
 
-        -- Disable built-in formatting since prettier is used
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-    end
+		-- Disable built-in formatting since prettier is used
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+	end,
 }
