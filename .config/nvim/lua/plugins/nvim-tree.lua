69b15855055f94b77d19c62517b3474c30f4806c
Spaxterr <spaxtersurvival@gmail.com>
2025-09-17 05:17:01 +0200

    replace neotree with nvimtree

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,155 @@
+return {
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = false,
+        config = function()
+            require("nvim-tree").setup({
+                auto_reload_on_write = true,
+                disable_netrw = true,
+                hijack_netrw = true,
+                hijack_directories = {
+                    enable = true,
+                    auto_open = true,
+                },
+                update_focused_file = {
+                    enable = true,
+                    update_root = false,
+                    ignore_list = {},
+                },
+                system_open = {
+                    cmd = nil,
+                    args = {},
+                },
+                diagnostics = {
+                    enable = false,
+                },
+                view = {
+                    float = {
+                        enable = true,
+                        quit_on_focus_loss = true,
+                        open_win_config = function()
+                            local screen_w = vim.opt.columns:get()
+                            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                            local window_w = 60
+                            local window_h = math.ceil(screen_h * 0.8 - 4)
+                            local window_w_int = math.floor(window_w)
+                            local window_h_int = math.floor(window_h)
+                            local center_x = (screen_w - window_w) / 2
+                            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+                            return {
+                                border = "rounded",
+                                relative = "editor",
+                                row = center_y,
+                                col = center_x,
+                                width = window_w_int,
+                                height = window_h_int,
+                                style = "minimal",
+                            }
+                        end,
+                    },
+                    width = 80,
+                    preserve_window_proportions = false,
+                    number = false,
+                },
+                renderer = {
+                    add_trailing = false,
+                    group_empty = true,
+                    highlight_git = true,
+                    full_name = false,
+                    highlight_opened_files = "name",
+                    highlight_modified = "icon",
+                    root_folder_label = ":~:s?$?/..?",
+                    indent_width = 2,
+                    indent_markers = {
+                        enable = true,
+                        inline_arrows = true,
+                        icons = {
+                            corner = "└",
+                            edge = "│",
+                            item = "│",
+                            bottom = "─",
+                            none = " ",
+                        },
+                    },
+                    icons = {
+                        webdev_colors = true,
+                        git_placement = "before",
+                        modified_placement = "after",
+                        padding = " ",
+                        symlink_arrow = " ➛ ",
+                        show = {
+                            file = true,
+                            folder = true,
+                            folder_arrow = true,
+                            git = true,
+                            modified = true,
+                        },
+                        glyphs = {
+                            default = "",
+                            symlink = "",
+                            bookmark = "󰆤",
+                            modified = "●",
+                            folder = {
+                                arrow_closed = "",
+                                arrow_open = "",
+                                default = "",
+                                open = "",
+                                empty = "",
+                                empty_open = "",
+                                symlink = "",
+                                symlink_open = "",
+                            },
+                            git = {
+                                unstaged = "✗",
+                                staged = "✓",
+                                unmerged = "",
+                                renamed = "➜",
+                                untracked = "★",
+                                deleted = "",
+                                ignored = "",
+                            },
+                        },
+                    },
+                    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "package.json" },
+                    symlink_destination = true,
+                },
+                hijack_cursor = true,
+                filters = {
+                    git_ignored = false,
+                    dotfiles = false,
+                    git_clean = false,
+                    no_buffer = false,
+                    custom = {},
+                    exclude = {},
+                },
+                filesystem_watchers = {
+                    enable = true,
+                    debounce_delay = 50,
+                    ignore_dirs = {},
+                },
+                git = {
+                    enable = true,
+                    ignore = false,
+                    show_on_dirs = true,
+                    show_on_open_dirs = true,
+                    timeout = 400,
+                },
+                modified = {
+                    enable = true,
+                    show_on_dirs = true,
+                    show_on_open_dirs = true,
+                },
+                tab = {
+                    sync = {
+                        open = false,
+                        close = false,
+                        ignore = {},
+                    },
+                },
+            })
+        end,
+    },
+}
