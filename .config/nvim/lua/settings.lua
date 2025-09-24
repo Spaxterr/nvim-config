62073bad5513ae3ada12caaabfd0c60cec7ad8df
Spaxterr <spaxtersurvival@gmail.com>
2025-09-24 06:24:07 +0200

    use absolute paths for WSL clipboard fix

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -99,14 +99,14 @@ if is_wsl then
     vim.g.clipboard = {
         name = 'WslClipboard',
         copy = {
-            ['+'] = 'clip.exe',
-            ['*'] = 'clip.exe',
+            ['+'] = '/mnt/c/Windows/System32/clip.exe',
+            ['*'] = '/mnt/c/Windows/System32/clip.exe',
         },
         paste = {
             ['+'] =
-            'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
             ['*'] =
-            'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
         },
         cache_enabled = 0,
     }
